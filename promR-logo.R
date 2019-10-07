1952f21777b34c053c78b04d7003af12b1c02bcd
Konrad Zdeb <konrad.zdeb@me.com>
2019-10-07 17:31:28 +0100

    Reverting to using svg

--- promR-logo.R
+++ promR-logo.R
@@ -2,11 +2,41 @@
 
 # Generate logo for the promR package
 
+# Settings ----------------------------------------------------------------
+
+# Libs
+library(hexSticker)
+library(magick)
+
 
 # Download ----------------------------------------------------------------
 
 # Download image, marked as for reuse and modification
-if (!file.exists("logo.svg")) {
+if (!file.exists("torch.svg")) {
     download.file(url = "https://svgsilh.com/svg/1133865.svg",
-                  destfile = "logo.svg")
+                  destfile = "torch.svg")
 }
+
+# Image -------------------------------------------------------------------
+
+torch <- image_read(here::here("torch.svg"))
+torch <- image_fill(torch, "orange", fuzz = 20, point = "+1+1")
+image_scale(torch, "75")
+
+
+# Package logo ------------------------------------------------------------
+
+sticker(
+    torch,
+    package = "promR",
+    p_size = 5,
+    h_fill = "#ffffff",
+    h_color = "#cc7000",
+    p_color = "#003366",
+    s_x = 1,
+    s_y = .75,
+    s_width = 1.3,
+    s_height = 1,
+    filename = here::here("logo.png"),
+    spotlight = FALSE
+)
